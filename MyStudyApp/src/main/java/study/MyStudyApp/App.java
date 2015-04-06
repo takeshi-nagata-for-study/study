e585e932e9103732c6bcf1d8ad1e5249a71ae2f3
takeshi-nagata-for-study <msg_nagata@hotmail.com>
2015-04-06 23:13:11 +0900

    new

---
+++ MyStudyApp/src/main/java/study/MyStudyApp/App.java
@@ -0,0 +1,13 @@
+package study.MyStudyApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
